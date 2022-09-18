b65afe47016fee811a79a265d803fca5b0eebcf9
Dimas Aditya <dimassuruf66@gmail.com>
2022-09-18 19:58:34 +0700

    promise done

--- promise.js
+++ promise.js
@@ -1,7 +1,27 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const bioskopIXX = await promiseTheaterIXX()
+  const bioskopVGC = await promiseTheaterVGC()
+  const jumlahCinema = [...bioskopIXX, ...bioskopVGC]
+
+  return new Promise ((resolve, reject) => {
+     if (jumlahCinema.length) {
+        var jumlahEmosi = 0;
+
+        for (cinema of jumlahCinema) {
+           if (cinema.hasil === emosi) {
+              jumlahEmosi += 1;
+           }
+        }
+
+        resolve(jumlahEmosi);
+     } else {
+        reject('Maaf tidak ada yang ditampilkan');
+     }
+  })
+};
 
 module.exports = {
   promiseOutput,
